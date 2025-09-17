1b0b05586a6844b932b9ed4dfc3889c5a90adee6
kang9805 <zzionkk@naver.com>
2025-09-17 19:26:21 +0900

    chapter_04 과제

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import todo_list, todo_info
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # todo
     path('todo/', todo_list, name='todo_list'),
+    path('todo/create/', todo_create, name='todo_create'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
+
+    # accounts
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', user_views.login, name='login'),
     path('accounts/signup/', user_views.signup, name='signup'),
